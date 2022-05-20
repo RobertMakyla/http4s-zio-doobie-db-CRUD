6e53f6eaeb7f614491d96179110b042104c47133
robert.makyla <robert.makyla@gmail.com>
2022-05-20 10:32:35 +0200

    add more specs to PortSpec

--- build.sbt
+++ build.sbt
@@ -10,13 +10,14 @@ libraryDependencies ++= {
   val zhttpVersion = "2.0.0-RC7" // https://repo1.maven.org/maven2/io/d11/
 
   Seq(
-    "dev.zio" %% "zio" % zioVersion,
-    // "dev.zio" %% "zio-test" % zioVersion,
-    "dev.zio" %% "zio-config" % zioConfigVersion,
+    "dev.zio" %% "zio"                 % zioVersion,
+    "dev.zio" %% "zio-test"            % zioVersion % "test",
+    "dev.zio" %% "zio-test-sbt"        % zioVersion % "test",
+    "dev.zio" %% "zio-config"          % zioConfigVersion,
     "dev.zio" %% "zio-config-typesafe" % zioConfigVersion,
-    "io.d11" %% "zhttp" % zhttpVersion,
-    // "io.d11" %% "zhttp-test" % zhttpVersion % Test,
-    "org.slf4j" % "slf4j-api" % "1.7.36"
+    "io.d11"  %% "zhttp"               % zhttpVersion,
+    // "io.d11" %% "zhttp-test"        % zhttpVersion % Test,
+    "org.slf4j" % "slf4j-api"          % "1.7.36"
   )
 }
 enablePlugins(JavaAppPackaging)
--- src/test/scala/zio_real_estate_crawler/http/PortSpec.scala
+++ src/test/scala/zio_real_estate_crawler/http/PortSpec.scala
@@ -10,19 +10,33 @@ import zio_real_estate_crawler.config.{AppConfig, HttpConfig, WelcomeMessage}
 import zio_real_estate_crawler.http.PortSpec.test
 
 import java.io.IOException
+
 object PortSpec extends ZIOSpecDefault {
 
-  def spec = suite("PortSpec")(
-
-    test("read port from the system property") {
-      for {
-        port <- ZIO.service[Port]
-      } yield assertTrue(port.value == 123)
-    }.provide(
-      ZLayer(ZIO.succeed(AppConfig(WelcomeMessage("hi test"), HttpConfig("testhost", 666)))),
-      TestSystem.live(Data(Map("http.port" -> "123"), Map(), "\n")),
-      Port.fromSystemPropOrConfig
+  def spec = {
+    suite("PortSpec")(
+      test("read port from the system property")(
+        assertM(ZIO.service[Port])(equalTo(Port(123)))
+      ).provide(
+        appConfigLayerWithPort666,
+        TestSystem.live(Data(Map("http.port" -> "123"), Map(), "\n")),
+        Port.fromSystemPropOrConfig
+      ),
+      test("read port from the config file if there's no property")(
+        assertM(ZIO.service[Port])(equalTo(Port(666)))
+      ).provide(
+        appConfigLayerWithPort666,
+        TestSystem.default,
+        Port.fromSystemPropOrConfig
+      ),
+      test("read port from the config file if property is incorrect")(
+        assertM(ZIO.service[Port])(equalTo(Port(666)))
+      ).provide(
+        appConfigLayerWithPort666,
+        TestSystem.live(Data(Map("http.port" -> "incorrect"), Map(), "\n")),
+        Port.fromSystemPropOrConfig
+      )
     )
-
-  )
+  }
+  val appConfigLayerWithPort666 = ZLayer(ZIO.succeed(AppConfig(WelcomeMessage("hi test"), HttpConfig("testhost", 666))))
 }
