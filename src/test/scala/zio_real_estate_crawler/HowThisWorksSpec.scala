4a29a18009ee118e1f67f42134d5572eb4947880
robert.makyla <robert.makyla@gmail.com>
2022-05-22 23:09:44 +0200

    add HowThisWorksSpec (testing envs layers)

---
+++ src/test/scala/zio_real_estate_crawler/HowThisWorksSpec.scala
@@ -0,0 +1,34 @@
+package zio_real_estate_crawler
+
+import zio.test.{TestConsole, TestRandom, TestSystem, ZIOSpecDefault, assertTrue}
+import zio.{Console, Random, System}
+
+object HowThisWorksSpec extends ZIOSpecDefault {
+
+  def spec = {
+
+    suite("built-in services")(
+      test("TestConsole") {
+        for {
+          _ <- Console.printLine("Hello, World!")
+          res <- TestConsole.output
+        } yield assertTrue(res == Vector("Hello, World!\n"))
+      },
+      test("TestSystem") {
+        for {
+          _ <- TestSystem.putProperty("k", "abc")
+          res <- System.property("k")
+        } yield assertTrue(res == Some("abc"))
+      },
+      test("TestRandom - use feedInts for concrete values, setSeed for stable values") {
+        for {
+          _ <- TestRandom.feedInts(3, 9, 2)
+          r1 <- Random.nextInt
+          r2 <- Random.nextInt
+          r3 <- Random.nextInt
+        } yield assertTrue(List(3, 9, 2) == List(r1, r2, r3))
+      }
+
+    )
+  }
+}
