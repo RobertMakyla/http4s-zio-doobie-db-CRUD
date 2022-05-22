52852eb2d6cfbe2ef0f5f468e0e7ce212b016dc2
robert.makyla <robert.makyla@gmail.com>
2022-05-22 23:09:18 +0200

    add AppConfig spec

--- src/test/scala/zio_real_estate_crawler/config/AppConfigSpec.scala
+++ src/test/scala/zio_real_estate_crawler/config/AppConfigSpec.scala
@@ -1,18 +1,33 @@
 package zio_real_estate_crawler.config
 
-import zio.ZIO
+import com.typesafe.config.ConfigFactory
+import zio.{ZIO, _}
+import zio.config.PropertyTreePath.Step
+import zio.config.ReadError.ConversionError
+import zio.test.Assertion._
+import zio.test.{ZIOSpecDefault, _}
 
-//object AppConfigSpec extends DefaultRunnableSpec {
-//
-//  suite("AppConfigSpec")(
-//    testM("loading welcome message"){
-//      assertM(ZIO.service[AppConfig])(
-//        equalTo(
-//          AppConfig(
-//            welcomeMessage = WelcomeMessage("Welcome to ZIO Real Estate - Web Crawler !"),
-//            http = HttpConfig("0.0.0.0", 8080))
-//        )
-//      )
-//    }
-//  )
-//}
+object AppConfigSpec extends ZIOSpecDefault {
+
+  val expected: ConversionError[String] =
+    ConversionError(List(Step.Key("changeme")), "Predicate isEmpty() did not fail.", Set.empty)
+
+  def spec = {
+    suite("AppConfigSpec")(
+      test("is loading correct config") {
+        assertM(ZIO.service[AppConfig])(
+          equalTo(
+            AppConfig(welcomeMessage = WelcomeMessage("Test welcome message"), http = HttpConfig("testhost", 1234))
+          )
+        )
+      }.provide(correctTestConfig.orDie)
+    )
+  }
+
+  val correctTestConfig: ZLayer[Any, Throwable, AppConfig] = configFromResource("test.conf")
+
+  def configFromResource(path: String) = ZLayer {
+    ZIO.attempt(ConfigFactory.load(path)).map(RawConfig(_))
+  } >>> AppConfig.fromRawConfig
+
+}
